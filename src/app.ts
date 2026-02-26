a771083854defa778c10f29b2d640287a3b6a9ff
Rahad Ullah <rahadullah10@gmail.com>
2026-02-26 09:35:03 +0600

    refactor(app.ts): extend limit to 50mb express json

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ app.use(
     credentials: true,
   }),
 );
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
